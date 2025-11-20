f322ef1451e2f51c6ef69d517c6c8af2be365920
JeissonDevelop <jei.gomez15@gmail.com>
2025-11-20 21:00:21 +0100

    entrega opcional

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,53 +1,56 @@
 import React from 'react';
 
 const Content: React.FC=() => {
+
     return (
-      <div className="tasks-container">
-        <div className="task-item">
-          <div className="task-image">
-            <img
-              alt="Francisca"
-              src="/assets/img/francisca.jpg"
-              className="task-image"
-            />
+        <>
+        <div className="tasks-container">
+          <div className="task-item">
+            <div className="task-image">
+              <img
+                alt="Francisca"
+                src="/assets/img/francisca.jpg"
+                className="task-image"
+              />
+            </div>
+            <span className="task-time"><i className="fas fa-fork-knife"></i> Hace una hora</span>
+            <p className="task-description">Fui a comer con amigos</p>
           </div>
-          <span className="task-time">Hace una hora</span>
-          <p className="task-description">Fui a comer con amigos</p>
-        </div>
-        <div className="task-item">
-          <div className="task-image">
-            <img
-              alt="Paco"
-              src="/assets/img/paco.jpg"
-              className="task-image"
-            />
+          <div className="task-item">
+            <div className="task-image">
+              <img
+                alt="Paco"
+                src="/assets/img/paco.jpg"
+                className="task-image"
+              />
+            </div>
+            <span className="task-time"><i className="fas fa-book"></i> 10:00 am</span>
+            <p className="task-description">Leí un artículo sobre tecnología</p>
           </div>
-          <span className="task-time">10:00 am</span>
-          <p className="task-description">Leí un artículo sobre tecnología</p>
-        </div>
-        <div className="task-item">
-          <div className="task-image">
-            <img
-              alt="Quica"
-              src="/assets/img/quica.jpg"
-              className="task-image"
-            />
+          <div className="task-item">
+            <div className="task-image">
+              <img
+                alt="Quica"
+                src="/assets/img/quica.jpg"
+                className="task-image"
+              />
+            </div>
+            <span className="task-time"><i className="fas fa-pen"></i> 10:00 am</span>
+            <p className="task-description">Escribí notas sobre un proyecto importante</p>
           </div>
-          <span className="task-time">10:00 am</span>
-          <p className="task-description">Escribí notas sobre un proyecto importante</p>
-        </div>
-        <div className="task-item">
-          <div className="task-image">
-            <img
-              alt="Curro"
-              src="/assets/img/curro.jpg"
-              className="task-image"
-            />
+          <div className="task-item">
+            <div className="task-image">
+              <img
+                alt="Curro"
+                src="/assets/img/curro.jpg"
+                className="task-image"
+              />
+            </div>
+            <span className="task-time"><i className="fas fa-file-powerpoint"></i> 2:21 pm</span>
+            <p className="task-description">Preparé la presentación para la reunión de mañana</p>
           </div>
-          <span className="task-time">2:21 pm</span>
-          <p className="task-description">Preparé la presentación para la reunión de mañana</p>
         </div>
-      </div>
+      </>
     );
   }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,23 @@
-export default function Header() {
+import React, { useState } from 'react';
+
+const Header: React.FC=() => {
+    const [searchTerm, setSearchTerm] = useState('');
   return (
     <header className="header">
       <h1>Registro de Tareas</h1>
+
+      <div className="search-container">
+          <input
+            type="text"
+            className="searchInput"
+            placeholder="Buscar ..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <i className="fas fa-search searchIcon"></i>
+        </div>
     </header>
   );
 }
+
+export default Header
